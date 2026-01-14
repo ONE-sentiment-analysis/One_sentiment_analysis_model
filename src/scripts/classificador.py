fe421b33670b49704213ffb81c6ea53b7ed7fee0
inatagan <22266171+inatagan@users.noreply.github.com>
2026-01-14 16:36:28 -0300

    analise do modelo completa

---
+++ src/scripts/classificador.py
@@ -0,0 +1,75 @@
+import argparse
+import re
+import nltk
+import logging
+from pathlib import Path
+from nltk.corpus import stopwords
+from joblib import load
+import sys
+
+# configuracao de caminhos com pathlib
+BASE_DIR = Path(__file__).parent.parent # vai para /src
+MODELS_DIR = BASE_DIR / "models"
+LOGS_DIR = BASE_DIR / "logs"
+
+# criar a pasta de logs caso ela nao exista
+LOGS_DIR.mkdir(exist_ok=True)
+
+# configuracao do logging
+logging.basicConfig(
+    filename=LOGS_DIR / "execucao.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - Modelo: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+# configuracao NLTK
+nltk.download('stopwords', quiet=True)
+stops = set(stopwords.words('portuguese'))
+
+def limpar_texto(texto):
+    texto = str(texto).lower()
+    texto = re.sub(r'[^a-zA-Záéíóúàèìòùâêîôûãõç\s]', '', texto)
+    texto = ' '.join([palavra for palavra in texto.split() if palavra not in stops])
+    return texto
+
+def carregar_recursos(nome_modelo):
+    try:
+        modelos_disponiveis = {
+            'nb': 'modelo_naive_bayes.joblib',
+            'lr': 'modelo_logistic_regression.joblib',
+            'rf': 'modelo_random_forest.joblib'
+        }
+        
+        caminho_modelo = MODELS_DIR / modelos_disponiveis[nome_modelo]
+        caminho_vetorizador = MODELS_DIR / 'vectorizer_tfidf.joblib'
+
+        return load(caminho_modelo), load(caminho_vetorizador)
+    except Exception as e:
+        logging.error(f"Erro ao carregar recursos: {e}")
+        print(f"Erro crítico: Verifique os logs em {LOGS_DIR}")
+        sys.exit(1)
+
+def main():
+    parser = argparse.ArgumentParser(description="CLI para Análise de Sentimento")
+    parser.add_argument("-t", "--text", type=str, required=True)
+    parser.add_argument("-m", "--model", type=str, required=True, choices=['nb', 'lr', 'rf'])
+    args = parser.parse_args()
+
+    modelo, vectorizer = carregar_recursos(args.model)
+    
+    texto_limpo = limpar_texto(args.text)
+    vetorizado = vectorizer.transform([texto_limpo])
+    
+    predicao = modelo.predict(vetorizado)[0]
+    probabilidade = modelo.predict_proba(vetorizado).max()
+
+    # logando a execucao
+    log_msg = f"{args.model.upper()} | Texto: '{args.text}' | Predição: {predicao} | Confiança: {probabilidade:.2%}"
+    logging.info(log_msg)
+
+    # output para o usuario
+    print(f"\nResultado ({args.model.upper()}): {predicao} ({probabilidade:.2%})")
+
+if __name__ == "__main__":
+    main()
